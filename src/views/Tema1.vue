40cc449bb501613e79eb05d9cc4f7eca5c729aeb
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2026-03-24 22:40:48 -0500

    avance

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -713,7 +713,7 @@
           .tarjeta.clr--pink.p-4.rounded-0.h-100.bg1
             .tarjeta.bg-white.p-4
               TarjetaAudio.color-acento-botones(
-              texto="Podcast: Importancia de los aceites esenciales"
+              texto="<i>Podcast</i>: Importancia de los aceites esenciales"
               :audio="require('@/assets/curso/Tema1/PODCAST_CF1_72210086.mp3')"
             )
               .indicador--click(v-if="mostrarIndicadorTarjetaAudio")
